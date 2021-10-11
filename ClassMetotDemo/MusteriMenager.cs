58b9def3bd0186255b42e930e702a01b7f0af4d0
yasir <yasir@DESKTOP-6QSJ7FE>
2021-10-12 00:05:14 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriMenager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriMenager
+    {
+        public void Ekle()
+        {
+            Console.WriteLine("Müsteriler Eklenmiştir.");
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri liste in musteriler)
+
+            {
+                Console.WriteLine(liste.Isim + " " + liste.soyIsim + " " + liste.Id);
+            }
+        }
+        public void Sil(Musteri musteri1)
+        {
+            Console.WriteLine(musteri1.Id + " " + musteri1.Isim + " " + musteri1.soyIsim + " " + "Kullanıcı silinmiştir.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 176852;
+            musteri1.Isim = "Murat";
+            musteri1.soyIsim = "Başdemir";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 789542;
+            musteri2.Isim = "Hasan";
+            musteri2.soyIsim = "Başdemir";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 654821;
+            musteri3.Isim = "Yasir";
+            musteri3.soyIsim = "Başdemir";
+
+            Musteri[] musteriler = new Musteri[] {musteri1,musteri2,musteri3};
+
+            MusteriMenager musteriMenager = new MusteriMenager();
+            musteriMenager.Ekle();
+            musteriMenager.Listele(musteriler);
+            musteriMenager.Sil(musteri3);
+        }
+    }
+}
